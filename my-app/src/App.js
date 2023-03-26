55c24e8f32c722a95d5d9900c18058069ea1b051
Godfrey1234 <godfrey555mabena@gmail.com>
2023-03-26 03:04:23 -0700

    updates

---
+++ my-app/src/App.js
@@ -0,0 +1,97 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+  
+<div>
+<section class="sec1 ">
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <div class="contents ">
+      <h1 class="h1" >I'M GODFREY MABENA </h1>
+      <p>Software Developer</p>
+      </div>
+
+      <div class="image  w-34 h-34">
+
+    
+      </div>
+   
+    </div>
+  
+</div>
+</div>
+
+</section>
+
+
+<section class="sec2 ">
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <div class="card">
+
+      <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  
+      </div>
+      </div>
+   
+    </div>
+  <br></br>
+    <div class="col-sm">
+    <div class="card">
+
+<div class="card-body">
+<h5 class="card-title">Card title</h5>
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+</div>
+</div>
+      
+   
+   
+   </div>
+
+
+   <div class="col-sm">
+   
+   <div class="card">
+
+    <div class="card-body">
+<h5 class="card-title">Card title</h5>
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+    </div>
+    </div>
+   
+   </div>
+  
+</div>
+</div>
+
+</section>
+
+
+
+</div>
+ 
+
+  
+
+
+
+
+ 
+
+
+  );
+}
+
+export default App;
